2ec384506124bccff3d04751299ade4aa0776818
noboru-i <ishikura.noboru@gmail.com>
2014-09-29 00:00:48 +0900

    登録の降順に変更

--- app/src/main/java/hm/orz/chaos114/android/slideviewer/model/TalkMetaData.java
+++ app/src/main/java/hm/orz/chaos114/android/slideviewer/model/TalkMetaData.java
@@ -3,8 +3,6 @@ package hm.orz.chaos114.android.slideviewer.model;
 
 import com.j256.ormlite.field.DatabaseField;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
@@ -14,15 +12,6 @@ public class TalkMetaData {
 
     @DatabaseField(foreign = true, foreignAutoRefresh = true, columnName = "talk_id")
     private Talk talk;
-
-    @DatabaseField
-    private Date createdAt;
-
-    @DatabaseField
-    private Date updatedAt;
-
-    @DatabaseField
-    private Date recentViewedAt;
 
     @DatabaseField
     private String title;
