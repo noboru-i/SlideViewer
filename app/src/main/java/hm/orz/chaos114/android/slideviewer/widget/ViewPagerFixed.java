8f3c67a06fd09632d0edd181892d39fc604454bb
noboru-i <ishikura.noboru@gmail.com>
2016-04-30 01:37:55 +0900

    enable zoom

---
+++ app/src/main/java/hm/orz/chaos114/android/slideviewer/widget/ViewPagerFixed.java
@@ -0,0 +1,39 @@
+package hm.orz.chaos114.android.slideviewer.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+public class ViewPagerFixed extends ViewPager {
+    private static final String TAG = ViewPagerFixed.class.getSimpleName();
+
+    public ViewPagerFixed(Context context) {
+        super(context);
+    }
+
+    public ViewPagerFixed(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            Log.w(TAG, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            Log.w(TAG, ex);
+        }
+        return false;
+    }
+}
