f434a4426dbc3fb7dda66a8be3a1fffe40c59f5e
noboru-i <ishikura.noboru@gmail.com>
2018-02-24 15:58:06 +0900

    work Dagger2

---
+++ app/src/main/java/hm/orz/chaos114/android/slideviewer/di/AppComponent.java
@@ -0,0 +1,30 @@
+package hm.orz.chaos114.android.slideviewer.di;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjectionModule;
+import hm.orz.chaos114.android.slideviewer.SlideViewerApplication;
+
+@Singleton
+@Component(modules = {
+        AndroidInjectionModule.class,
+        OcrModule.class,
+        AppModule.class,
+        ActivityModule.class
+})
+public interface AppComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder application(Application application);
+        Builder ocrModule(OcrModule ocrModule);
+
+        AppComponent build();
+    }
+
+    void inject(SlideViewerApplication app);
+}
---
+++ app/src/main/java/hm/orz/chaos114/android/slideviewer/di/OcrModule.java
@@ -0,0 +1,20 @@
+package hm.orz.chaos114.android.slideviewer.di;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import hm.orz.chaos114.android.slideviewer.ocr.OcrUtil;
+
+@Module
+public class OcrModule {
+    public static OcrModule instance = new OcrModule();
+
+    @Singleton
+    @Provides
+    public OcrUtil provideOcrUtil(Application app) {
+        return new OcrUtil(app);
+    }
+}
